5c793213d69cdf06f86d01b70865e67aa80dea60
Xcheniti <xcheniti@gmail.com>
2026-01-16 03:45:16 +0100

    Update streaming links and comments in update_stream.py

--- update_stream.py
+++ update_stream.py
@@ -1,10 +1,10 @@
-# كود بسيط جداً لإنشاء قائمة تشغيل نظيفة
+# روابط بديلة تمر عبر سيرفرات عالمية (Cloudflare)
 content = """#EXTM3U
-#EXTINF:-1, Echorouk News HD
-https://shls-echorouk-news.v7.vcloud.dz/echorouk_news/index.m3u8
+#EXTINF:-1, Echorouk News
+https://all-dz-news.pages.dev/streams/echorouk_news.m3u8
 
 #EXTINF:-1, Echorouk TV
-https://shls-echorouk-tv.v7.vcloud.dz/echorouk_tv/index.m3u8
+https://all-dz-news.pages.dev/streams/echorouk_tv.m3u8
 """
 
 with open("playlist.m3u", "w", encoding="utf-8") as f:
