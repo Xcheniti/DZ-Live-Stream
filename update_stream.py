960a9e17e8f248ef5c618ef99207a6ba694a57dc
Xcheniti <xcheniti@gmail.com>
2026-01-16 03:31:40 +0100

    Refactor playlist generation in update_stream.py
    Simplified playlist creation by using a content variable.

--- update_stream.py
+++ update_stream.py
@@ -1,10 +1,11 @@
+# كود بسيط جداً لإنشاء قائمة تشغيل نظيفة
+content = """#EXTM3U
+#EXTINF:-1, Echorouk News HD
+https://shls-echorouk-news.v7.vcloud.dz/echorouk_news/index.m3u8
+
+#EXTINF:-1, Echorouk TV
+https://shls-echorouk-tv.v7.vcloud.dz/echorouk_tv/index.m3u8
+"""
+
 with open("playlist.m3u", "w", encoding="utf-8") as f:
-    f.write("#EXTM3U\n\n")
-    
-    # رابط قناة الشروق نيوز (مصدر بديل مفتوح)
-    f.write("#EXTINF:-1, Echorouk News HD\n")
-    f.write("https://shls-echorouk-news.v7.vcloud.dz/echorouk_news/index.m3u8\n\n")
-    
-    # رابط قناة الشروق العامة (إضافية)
-    f.write("#EXTINF:-1, Echorouk TV\n")
-    f.write("https://shls-echorouk-tv.v7.vcloud.dz/echorouk_tv/index.m3u8\n")
+    f.write(content)
